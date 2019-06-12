681efb57ec0508d834b5c449640e3bd24906ae96
Mukil Loganathan <mukilloganathan@gmail.com>
2019-06-11 22:49:02 -0700

    few fixes

--- selfplusplus/settings.py
+++ selfplusplus/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'media')
 MEDIA_URL = '/media/' # django-storages
